846f0ed3cbba263ad897a46068d3b19529bfd298
Your Name <jasjohn3@uat.edu>
2021-08-19 17:43:13 -0700

    Adding fetch response to Animals.js

--- server.js
+++ server.js
@@ -97,9 +97,27 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+// _______________________________ Client Side GET Requests_______________________________
+// Loads the html files for the client side
 app.get('/',(req,res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/animals', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req,res) => {
+  res.sendFile(path.join(__dirname,'./public/zookeepers.html'));
+});
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// _______________________________ Client Side GET Requests_______________________________
+
+// _______________________________ PORT listener_______________________________
+// Starts the server to begin listening for requests
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
